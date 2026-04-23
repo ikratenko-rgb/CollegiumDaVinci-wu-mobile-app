6c70ecd18b48077acd2c2f088ec661ad11606c9c
ikratenko-rgb <ikratenko@edu.cdv.pl>
2026-04-23 06:06:50 +0200

    mobile ui fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wu-schedule-v4';
+const CACHE_NAME = 'wu-schedule-v5';
 const STATIC_ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
